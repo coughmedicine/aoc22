7cc4d826e4fe83df9de990a17942d52d9426db63
Clara H <72562074+coughmedicine@users.noreply.github.com>
2022-12-02 00:18:37 +0000

    added linter configuration and formatter

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -7,19 +7,19 @@ console.log("cheems");
 const input = await fs.readFile("src/day1/input.txt", "utf-8");
 const lines = input.split("\n");
 
-var sums: number[] = [];
-var cur = 0;
+const sums: number[] = [];
+let cur = 0;
 
 lines.forEach((element) => {
-  if (element == "") {
-    sums.push(cur);
-    cur = 0;
-  } else {
-    cur += parseInt(element, 10);
-  }
+    if (element == "") {
+        sums.push(cur);
+        cur = 0;
+    } else {
+        cur += parseInt(element, 10);
+    }
 });
 if (cur !== 0) {
-  sums.push(cur);
+    sums.push(cur);
 }
 
 sums.sort((a, b) => b - a);
